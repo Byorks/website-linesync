7c862233b25a617a8bd1282c1fee4d865d5d23fd
Byork <vanessabyorksp@gmail.com>
2025-11-14 10:58:48 -0300

    :contruction: inprog: header

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// Swiper config
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+
+// Lucide
+lucide.createIcons();
