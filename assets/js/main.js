1e8fe8ce762d3eb90738b4389b62f55c3003481c
Byork <vanessabyorksp@gmail.com>
2025-11-21 15:11:52 -0300

    :sparkles: feat: header menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,59 @@ const carrosselHomeCases = new Swiper(".swiper-cases", {
 // Lucide
 lucide.createIcons();
 
+// __________________________________________
+// __________________________________________
+// Nav Menu
+
+const menu = document.getElementById("menu");
+const navBtn = document.getElementById("nav-btn");
+const overlay = document.getElementById("menu-overlay");
+
+// Troca o ícone do lucide
+const updateIcon = (isOpen) => {
+  navBtn
+    .querySelector("svg")
+    .setAttribute("data-lucide", isOpen ? "x" : "menu");
+  lucide.createIcons();
+};
+
+const closeMobileMenu = () => {
+  menu.classList.remove("top-[66px]");
+  menu.classList.add("top-[-300%]");
+  overlay.classList.remove("opacity-100", "pointer-events-auto");
+  overlay.classList.add("opacity-0", "pointer-events-none");
+  menu.dataset.state = "closed";
+  updateIcon(false);
+};
+
+const openMobileMenu = () => {
+  menu.classList.add("top-[66px]");
+  menu.classList.remove("top-[-300%]");
+  overlay.classList.remove("opacity-0", "pointer-events-none");
+  overlay.classList.add("opacity-100", "pointer-events-auto");
+  menu.dataset.state = "open";
+  updateIcon(true);
+};
+
+navBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  menu.dataset.state === "open" ? closeMobileMenu() : openMobileMenu();
+
+  // tamanho do navbar
+  // menu.classList.toggle('top-[66px]');
+});
+
+// Fechar quando clica fora do menu
+document.addEventListener("click", (e) => {
+  const clickedInside = navBtn.contains(e.target) || menu.contains(e.target);
+  if (!clickedInside && menu.dataset.state === "open") {
+    closeMobileMenu();
+  }
+});
+
+// fechando com tecla ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && menu.dataset.state === "open") {
+    closeMobileMenu();
+  }
+});
